1a5be5c27a3b845d713da075677b9dc3ee791172
raihan <rishaon0@gmail.com>
2025-07-16 05:59:50 +0600

    feat: :zap: add vercel web analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Hanken_Grotesk } from "next/font/google";
 import type React from "react";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const display = Hanken_Grotesk({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={display.className}>
         <Providers>{children}</Providers>
+        <Analytics />
       </body>
     </html>
   );
