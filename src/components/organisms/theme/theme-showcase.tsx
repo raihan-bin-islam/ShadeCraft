3dbf38d2c25d01532610d69f98807e367f190d20
Raihan Bin Islam <97444173+raihan-bin-islam@users.noreply.github.com>
2025-08-09 20:54:49 +0600

    Update generator button text

--- src/components/organisms/theme/theme-showcase.tsx
+++ src/components/organisms/theme/theme-showcase.tsx
@@ -154,7 +154,7 @@ export function ThemeShowcase({
           />
           <div className="hidden md:flex items-center gap-3">
             <ShinyButton onClick={() => generateSingle({ feelId: selectedFeel, fontClass: selectedFont, toneId: selectedTone })}>
-              <Sparkles /> Generate
+              <Sparkles /> Randomize
             </ShinyButton>
             <Separator />
             <EditorModal isDark={isDark} theme={theme} isEditOpen={isEditOpen} setIsEditOpen={setIsEditOpen} />
